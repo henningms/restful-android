ddf120b0b264bb3205857e9c9216c7feecba1380
vladislavbakan <vladislav.bakan@gmail.com>
2013-03-05 16:06:24 +0300

    Response error parsing

--- src/no/henning/restful/http/status/HttpRestResponse.java
+++ src/no/henning/restful/http/status/HttpRestResponse.java
@@ -1,30 +1,25 @@
 package no.henning.restful.http.status;
 
-public class HttpRestResponse
-{
+public class HttpRestResponse {
 	private final int statusCode;
 	private final String statusReason;
 	private final String response;
-	
-	public HttpRestResponse(int statusCode, String statusReason, String response)
-	{
+
+	public HttpRestResponse(int statusCode, String statusReason, String response) {
 		this.statusCode = statusCode;
 		this.statusReason = statusReason;
 		this.response = response;
 	}
 
-	public int getStatusCode()
-	{
+	public int getStatusCode() {
 		return statusCode;
 	}
 
-	public String getStatusReason()
-	{
+	public String getStatusReason() {
 		return statusReason;
 	}
 
-	public String getResponse()
-	{
+	public String getResponse() {
 		return response;
 	}
 }
